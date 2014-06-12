288e4882f6cebcb7a9665e3720478f9b3e1d6dc8
Gabriel <gabrielbeaulieu117@gmail.com>
2014-06-12 15:51:35 -0400

    Jade header templating
    Gabriel Beaulieu

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 // Database
 var mongo = require('mongoskin');
-var db = mongo.db("mongodb://localhost:27017/Tutorial1", {native_parser:true});
+var db = mongo.db("mongodb://localhost:27017/EZ-Food", {native_parser:true});
 
 var app = express();
 

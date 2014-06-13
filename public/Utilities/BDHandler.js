22c3b60b88266bc4949aa2bb794fae5eb7b305b1
fvgoulet <flx890@gmail.com>
2014-06-13 01:52:10 -0500

    Fixed minor issues in Account and DBHandler

--- public/Utilities/BDHandler.js
+++ public/Utilities/BDHandler.js
@@ -3,7 +3,7 @@
  */
 
 var accountClass = require("Account");
-var categoryClass = retuire("Category")
+var categoryClass = require("Category")
 var mongo = require('mongoskin');
 
 function DBHandler()
@@ -12,14 +12,14 @@ function DBHandler()
     this.Construct = function()
     {
         this.db = mongo.db("mongodb://localhost:27017/EZ-Food", {native_parser:true});
-    }
+    };
 
     this.selectAccount = function(username)
     {
         var jsonUsername = {username: username};
         var account = new accountClass.Account();
         return account.Construct(db.get("Accounts").find(jsonUsername))
-    }
+    };
 
     // Insert a new account, true if added, false if username already exists
     this.insertAccount = function(account)
@@ -34,20 +34,13 @@ function DBHandler()
             return false;
         }
 
-    }
+    };
 
     //Find if the account exists
     this.accountExist = function(account)
     {
-        if(db.collection("Accounts").find(account.getJSONUsername()) != {})
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+        return db.collection("Accounts").find(account.getJSONUsername()) != {};
+    };
 
     //Save an account to the DB
     this.updateAccount = function(account)
@@ -61,7 +54,7 @@ function DBHandler()
         {
             return false;
         }
-    }
+    };
 
     //return an instance of Category
     this.getCategory = function(categoryId)
@@ -69,7 +62,7 @@ function DBHandler()
         var jsonCategoryId = {categoryId: categoryId};
         var category = new categoryClass.Category();
         return category.Construct(db.collection("Categories").find(jsonCategoryId))
-    }
+    };
 }
 
 module.exports.DBHandler = DBHandler;

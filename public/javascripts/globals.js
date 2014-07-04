7d05eaaf9abe963ffb7fd9834824fb87ee7f04fc
Gabriel <gabrielbeaulieu117@gmail.com>
2014-07-04 09:13:13 -0400

    Style and Javascript/JQuery

--- public/javascripts/globals.js
+++ public/javascripts/globals.js
@@ -7,6 +7,9 @@ $(document).ready(function() {
 
     $('#btnSubmitModifications').on('click', validateAccountFields);
 
+    $('#userPassword').on('keyup', PasswordValidation);
+
+    $('#confirmPassword').on('keyup', PasswordValidation);
 
 
 });
@@ -14,96 +17,87 @@ $(document).ready(function() {
 
 // Functions =============================================================
 
+
+function PasswordValidation(event){
+    var userPassword = document.getElementById('userPassword').value ;
+    var confirmation = document.getElementById('confirmPassword').value ;
+
+    if(confirmation != ''){
+        if(userPassword == confirmation){
+            $('#userPassword').css("backgroundColor", "green");
+            $('#confirmPassword').css("backgroundColor", "green");
+            $('#btnSubmitAccount').prop('disabled', false);
+        }
+        else{
+            $('#userPassword').css("backgroundColor", "red");
+            $('#confirmPassword').css("backgroundColor", "red");
+            $('#btnSubmitAccount').prop('disabled', true);
+        }
+    }
+    else{
+        $('#userPassword').css("backgroundColor", "white");
+        $('#confirmPassword').css("backgroundColor", "white");
+        $('#btnSubmitAccount').prop('disabled', true);
+    }
+
+
+}
+
 // validateAccountFields
 function validateAccountFields(event) {
     event.preventDefault();
-    // Set the max length of each fields.
-    var MAX_FIELD_LENGTH = 30;
 
-    // Set the min length of the username.
-    var MIN_USERNAME_LENGTH = 4;
-
-    // Set the regex for the format validation.
     var regexTextOnly = /[a-zA-Z]/;
     var regexEmail =/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/;
     var regexPhoneNumber =/[1-9]\d{2}-\d{3}-\d{4}/;
     var regexStartWithNumber =/\d\w*/;
     var regexDate = /[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])/;
+    var MIN_USERNAME_LENGTH = 4;
+    var username = document.getElementById('username').value ;
+    var firstName = document.getElementById('userFirstName').value ;
+    var secondName = document.getElementById('userSecondName').value ;
+    var birthDate = document.getElementById('userBirthDate').value ;
+    var email = document.getElementById('userEmail').value ;
+    var phoneNumber = document.getElementById('userPhoneNumber').value ;
+    var civicNumber = document.getElementById('userCivicNumber').value ;
+    var errorMessage = '';
 
-    // Check if all fields are filled.
-    var emptyErrorCount = 0;
-    var lengthErrorCount = 0;
-    $('#createAccountFields input').each(function(index, val) {
-        if($(this).val() === '') {
-            emptyErrorCount++;
-        }
-
-        if(MAX_FIELD_LENGTH < $(this).val().length) {
-            lengthErrorCount++;
-        }
-    });
-    // Show an error if a field is not filled.
-    if(0 != emptyErrorCount)
+    // Check if the format of all fields are okay.
+    if(MIN_USERNAME_LENGTH > username.toString().length)
     {
-        alert('Please fill all the fields.');
+        errorMessage += '<li>The Username must be longer than ' + (MIN_USERNAME_LENGTH - 1).toString() + '.</li>';
     }
-    else if(0 != lengthErrorCount)
+    if(false == regexTextOnly.test(firstName))
     {
-        alert('One of your field is longer than ' + MAX_FIELD_LENGTH.toString() + '.');
+        errorMessage += '<li>The first name should contain only letters.</li>';
+    }
+    if(false == regexTextOnly.test(secondName))
+    {
+        errorMessage += '<li>The second name should contain only letters.</li>';
+    }
+    if(false == regexEmail.test(email))
+    {
+        errorMessage += '<li>The email need to be valid.</li>';
+    }
+    if(false == regexPhoneNumber.test(phoneNumber))
+    {
+
+    }
+    if(false == regexStartWithNumber.test(civicNumber))
+    {
+        errorMessage += '<li>The civic number should start with a number.</li>';
+    }
+    if(false == regexDate.test(birthDate))
+    {
+        errorMessage += '<li>The birth date need to be on format : YYYY-MM-DD </li>';
+    }
+
+    if(errorMessage != ''){
+        document.getElementById('error').innerHTML = '<br/>Check the following: <br/> <ul>'+errorMessage;+'</ul>';
     }
     else
     {
-        var username = document.getElementsByName('username')[0].value ;
-        //var password = document.getElementsByName('userPassword')[0].value ;
-        var firstName = document.getElementsByName('userFirstName')[0].value ;
-        var secondName = document.getElementsByName('userSecondName')[0].value ;
-        var birthDate = document.getElementsByName('userBirthDate')[0].value ;
-        var email = document.getElementsByName('userEmail')[0].value ;
-        var phoneNumber = document.getElementsByName('userPhoneNumber')[0].value ;
-        var civicNumber = document.getElementsByName('userCivicNumber')[0].value ;
-        //var appartment = document.getElementsByName('userAppNumber')[0].value ;
-        //var street = document.getElementsByName('userStreet')[0].value ;
-        //var city = document.getElementsByName('userCity')[0].value ;
-        //var province = document.getElementsByName('userProvince')[0].value ;
-        //var zipCode = document.getElementsByName('userZipCode')[0].value ;
-
-
-
-        // Check if the format of all fields are okay.
-        if(MIN_USERNAME_LENGTH > username.toString().length)
-        {
-            alert('The Username must be longer than ' + (MIN_USERNAME_LENGTH - 1).toString() + '.');
-        }
-        else if(false == regexTextOnly.test(firstName))
-        {
-            alert('The first name should contain only letters.');
-        }
-        else if(false == regexTextOnly.test(secondName))
-        {
-            alert('The second name should contain only letters.');
-        }
-        else if(false == regexEmail.test(email))
-        {
-            alert('The email need to be valid.');
-        }
-        else if(false == regexPhoneNumber.test(phoneNumber))
-        {
-            alert('The phone number need to be on format : XXX-XXX-XXXX');
-        }
-        else if(false == regexStartWithNumber.test(civicNumber))
-        {
-            alert('The civic number should start with a number.');
-        }
-        else if(false == regexDate.test(birthDate))
-        {
-            alert('The birth date need to be on format : YYYY-MM-DD .');
-        }
-        else
-        {
-            // Everything is okay, go to the confirmation page.
-            document.account.submit();
-        }
-
+        document.account.submit();
     }
 };
 

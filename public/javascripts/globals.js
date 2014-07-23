6758b2435151c3b467eb21c353d10db80c7f7513
Gabriel <gabrielbeaulieu117@gmail.com>
2014-07-22 20:37:24 -0400

    Commit before merge with Beauchamps' code

--- public/javascripts/globals.js
+++ public/javascripts/globals.js
@@ -346,3 +346,27 @@ function selectAll(selectBox,selectAll) {
         }
     }
 }
+
+function createMenu(){
+    var xmlhttp;
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    // Callback on response.e
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            document.getElementById("content").innerHTML = xmlhttp.responseText;
+
+        }
+    }
+
+    xmlhttp.open("GET","/createAccount",true);
+    xmlhttp.send();
+}

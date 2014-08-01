012a6ddda9b2f96b4b469efbac66d1636ba6d6aa
Félix Vallières Goulet <flx890@gmail.com>
2014-08-01 00:10:42 -0400

    Added changeOrderStatus(id, status) in clientOrdersManagement.js

--- public/javascripts/clientOrdersManagement.js
+++ public/javascripts/clientOrdersManagement.js
@@ -23,4 +23,28 @@ function showOrderManagement()
     xmlhttp.send();
 }
 
+function changeOrdeStatus(id, status)
+{
+    var xmlhttp;
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlhttp.open("POST","/ordersManagement/updateOrderStatus",true);
+    xmlhttp.send('{"order_id":"' + id + '"}, {"order_status":"' + status + '"}');
+
+    // Callback on response.e
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            document.getElementById("content").innerHTML = xmlhttp.responseText;
+        }
+    };
+}
+
 
